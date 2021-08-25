af28354fbe062c75c25537faaa7a7144b93aecb6
Phuc Hoang <hvphuc081299@gmail.com>
2021-08-26 01:10:59 +0700

    add firebase

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDu6sHt-St8UQDvWYTtZR6qTyi7YSmxKJA",
+  authDomain: "demokhweb.firebaseapp.com",
+  projectId: "demokhweb",
+  storageBucket: "demokhweb.appspot.com",
+  messagingSenderId: "131478536589",
+  appId: "1:131478536589:web:b33bfd66bf16039d523dc4",
+  measurementId: "G-478Y9ZG94H",
+};
+
+firebase.initializeApp(firebaseConfig);
 
 function App() {
+  const handleSignIn = () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    firebase
+      .auth()
+      .signInWithPopup(provider)
+      .then((result) => {
+        console.log({ result });
+      })
+      .catch((error) => {
+        console.log({ error });
+      });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="d-flex justify-content-center align-items-center vh-100">
+      <button onClick={handleSignIn}>Sign in with Google</button>
     </div>
   );
 }
